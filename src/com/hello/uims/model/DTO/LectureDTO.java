34f9e8ace05d42a0a6d2030e95e4194fa9a76b5f
ASUS <ASUS@DESKTOP-PQT70BQ>
2022-11-11 17:16:19 +0900

    2022-11-11 LectureDTO, ProfessorDTO, StudentDTO

--- src/com/hello/uims/model/DTO/LectureDTO.java
+++ src/com/hello/uims/model/DTO/LectureDTO.java
@@ -2,4 +2,108 @@ package com.hello.uims.model.DTO;
 
 public class LectureDTO {
 
+	private int lectureNo; // 강의 번호
+	private String deptCode; // 학과 코드
+	private int profNo; // 교수 번호
+	private String lectureName; // 강의명
+	private char credit; // 수강학점
+	private int currNo; // 수강인원
+	private int fullNo; // 정원인원
+	private String day; // 요일
+	private String time; // 시간
+
+	public LectureDTO() {
+
+	}
+
+	public LectureDTO(int lectureNo, String deptCode, int profNo, String lectureName, char credit, int currNo,
+			int fullNo, String day, String time) {
+		this.lectureNo = lectureNo;
+		this.deptCode = deptCode;
+		this.profNo = profNo;
+		this.lectureName = lectureName;
+		this.credit = credit;
+		this.currNo = currNo;
+		this.fullNo = fullNo;
+		this.day = day;
+		this.time = time;
+	}
+
+	public int getLectureNo() {
+		return lectureNo;
+	}
+
+	public void setLectureNo(int lectureNo) {
+		this.lectureNo = lectureNo;
+	}
+
+	public String getDeptCode() {
+		return deptCode;
+	}
+
+	public void setDeptCode(String deptCode) {
+		this.deptCode = deptCode;
+	}
+
+	public int getProfNo() {
+		return profNo;
+	}
+
+	public void setProfNo(int profNo) {
+		this.profNo = profNo;
+	}
+
+	public String getLectureName() {
+		return lectureName;
+	}
+
+	public void setLectureName(String lectureName) {
+		this.lectureName = lectureName;
+	}
+
+	public char getCredit() {
+		return credit;
+	}
+
+	public void setCredit(char credit) {
+		this.credit = credit;
+	}
+
+	public int getCurrNo() {
+		return currNo;
+	}
+
+	public void setCurrNo(int currNo) {
+		this.currNo = currNo;
+	}
+
+	public int getFullNo() {
+		return fullNo;
+	}
+
+	public void setFullNo(int fullNo) {
+		this.fullNo = fullNo;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String toString() {
+		return "[lectureNo=" + lectureNo + ", deptCode=" + deptCode + ", profNo=" + profNo + ", lectureName="
+				+ lectureName + ", credit=" + credit + ", currNo=" + currNo + ", fullNo=" + fullNo + ", day=" + day
+				+ ", time=" + time + "]";
+	}
 }
--- src/com/hello/uims/model/DTO/ProfessorDTO.java
+++ src/com/hello/uims/model/DTO/ProfessorDTO.java
@@ -1,5 +1,121 @@
 package com.hello.uims.model.DTO;
 
+import java.sql.Date;
+
 public class ProfessorDTO {
 
+	private int profNo; // 교수 번호
+	private String deptCode; // 학과 코드
+	private String profId; // 교수 아이디
+	private String profPwd; // 교수 비밀번호
+	private String profName; // 교수 이름
+	private String address; // 교수 주소
+	private String profTelNo; // 교수 전화번호
+	private String email; // 교수 이메일
+	private Date enrollDate; // 교수 입사일
+	private char status; // 재직상태
+
+	public ProfessorDTO() {
+
+	}
+
+	public ProfessorDTO(int profNo, String deptCode, String profId, String profPwd, String profName, String address,
+			String profTelNo, String email, Date enrollDate, char status) {
+		this.profNo = profNo;
+		this.deptCode = deptCode;
+		this.profId = profId;
+		this.profPwd = profPwd;
+		this.profName = profName;
+		this.address = address;
+		this.profTelNo = profTelNo;
+		this.email = email;
+		this.enrollDate = enrollDate;
+		this.status = status;
+	}
+
+	public int getProfNo() {
+		return profNo;
+	}
+
+	public void setProfNo(int profNo) {
+		this.profNo = profNo;
+	}
+
+	public String getDeptCode() {
+		return deptCode;
+	}
+
+	public void setDeptCode(String deptCode) {
+		this.deptCode = deptCode;
+	}
+
+	public String getProfId() {
+		return profId;
+	}
+
+	public void setProfId(String profId) {
+		this.profId = profId;
+	}
+
+	public String getProfPwd() {
+		return profPwd;
+	}
+
+	public void setProfPwd(String profPwd) {
+		this.profPwd = profPwd;
+	}
+
+	public String getProfName() {
+		return profName;
+	}
+
+	public void setProfName(String profName) {
+		this.profName = profName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getProfTelNo() {
+		return profTelNo;
+	}
+
+	public void setProfTelNo(String profTelNo) {
+		this.profTelNo = profTelNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getEnrollDate() {
+		return enrollDate;
+	}
+
+	public void setEnrollDate(Date enrollDate) {
+		this.enrollDate = enrollDate;
+	}
+
+	public char getStatus() {
+		return status;
+	}
+
+	public void setStatus(char status) {
+		this.status = status;
+	}
+
+	public String toString() {
+		return "[profNo=" + profNo + ", deptCode=" + deptCode + ", profId=" + profId + ", profPwd=" + profPwd
+				+ ", profName=" + profName + ", address=" + address + ", profTelNo=" + profTelNo + ", email=" + email
+				+ ", enrollDate=" + enrollDate + ", status=" + status + "]";
+	}
 }
