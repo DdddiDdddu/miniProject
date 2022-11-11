543f12c5419569842c0f854afb8be14065db3c3d
ASUS <ASUS@DESKTOP-PQT70BQ>
2022-11-11 16:48:01 +0900

    2022-11-11 기본틀 추가(템플릿, 매퍼 설정)

--- src/com/hello/common/Template.java
+++ src/com/hello/common/Template.java
@@ -1,5 +1,37 @@
 package com.hello.common;
 
+import org.apache.ibatis.datasource.pooled.PooledDataSource;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+
 public class Template {
+
+	private static String DRIVER = "oracle.jdbc.driver.OracleDriver";
+	private static String URL = "jdbc:oracle:thin:@localhost:1521:xe"; // 자르 가져와서 쓸거면 이걸로
+	private static String USER = "greedy"; // 확인
+	private static String PASSWORD = "greedy"; // 확인
+	private static SqlSessionFactory sqlSessionFactory;
+
+	public static SqlSession getSqlSession() {
+
+		if (sqlSessionFactory == null) {
+			
+			Environment environment = new Environment("dev", new JdbcTransactionFactory(),
+					new PooledDataSource(DRIVER, URL, USER, PASSWORD));
+
+			Configuration configuration = new Configuration(environment);
+
+			configuration.addMapper(UimsMapper.class);
+
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+
+		}
+		
+		return sqlSessionFactory.openSession(false);
+	}
 
 }
