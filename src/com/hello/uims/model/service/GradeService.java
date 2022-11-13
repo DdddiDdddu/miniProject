dc4be4b6ead6cf5e6c007b5e169cfcea0208578f
os133 <os133@218.154.3.58>
2022-11-13 15:08:38 +0900

    2022-11-13 학생용 학점조회 추가

--- src/com/hello/uims/model/service/GradeService.java
+++ src/com/hello/uims/model/service/GradeService.java
@@ -1,11 +1,29 @@
 package com.hello.uims.model.service;
 
+import java.util.ArrayList;
+import org.apache.ibatis.session.SqlSession;
+
+import com.hello.common.UimsMapper;
+import com.hello.uims.model.DTO.GradeDTO;
+import static com.hello.common.Template.getSqlSession;
+
 public class GradeService {
+	
+	SqlSession sqlSession = null;
 	
 	public GradeService() { // 승재형 파트 
 
+	}
 
+	public ArrayList<GradeDTO> gradeCheck(int studentNo) {
 		
+		sqlSession = getSqlSession();
+		UimsMapper mapper = sqlSession.getMapper(UimsMapper.class);
+		ArrayList<GradeDTO> list = mapper.gradeCheck(studentNo);
+		
+		sqlSession.close();
+		
+		return list;
 	}
 
 }
--- src/com/hello/uims/view/PrintResult.java
+++ src/com/hello/uims/view/PrintResult.java
@@ -1,6 +1,19 @@
 package com.hello.uims.view;
 
+import java.util.List;
+
+import com.hello.uims.model.DTO.GradeDTO;
+
 public class PrintResult {
+
+	public void printGrade(List<GradeDTO> list) {
+		
+		System.out.println("========================================================================");
+		for(GradeDTO grade : list) {
+			System.out.println(grade);
+		}
+		
+	}
 
 //	public void printMenuList(List<MenuDTO> menuList) {
 //
@@ -35,34 +48,30 @@ public class PrintResult {
 //		
 //	}
 //
-//	public void printErrorMessage(String errorCode) {// 각 메소드에서 호출시케이스마다 추가하면서 하자
-//
-//		String errorMessage = "";
-//		
-//		switch (errorCode) {
+	public void printErrorMessage(String errorCode) {// 각 메소드에서 호출시케이스마다 추가하면서 하자
+
+		String errorMessage = "";
+		
+		switch (errorCode) {
 //		case "selectList":
 //			errorMessage = "메뉴 목록 조회에 실패하셨습니다.";
 //			break;
-//			
 //		case "selectOne":
 //			errorMessage = "메뉴 조회에 실패하셨습니다.";
 //			break;
-//			
 //		case "insert":
 //			errorMessage = "신규 메뉴 등록에 실패하셨습니다.";
 //			break;
-//			
-//		case "update":
-//			errorMessage = "메뉴 수정에 실패하셨습니다.";
-//			break;
-//			
+		case "gradeCheck":
+			errorMessage = "수강중인 강의가 없습니다.";
+			break;
 //		case "delete":
 //			errorMessage = "메뉴 삭제에 실패하셨습니다.";
 //			break;
-//		}
-//
-//		System.out.println(errorMessage);
-//		
-//	}
+		}
+		System.out.println("========================================================================");
+		System.out.println(errorMessage);
+		
+	}
 	
 }
